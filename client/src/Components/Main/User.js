040da93a877214569b690a3dadf4310e19c5b166
Eric Yi <erickanginyi@gmail.com>
2022-01-11 17:30:30 -0500

    Create files and import code

---
+++ client/src/Components/Main/User.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+
+function User({username, id}) {
+   //This is for comments length
+   const [commentsData, setCommentsData] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:9292/comments")
+         .then(resp => resp.json())
+         .then(data => setCommentsData(data));
+   }, []);
+   
+   const checkComments = commentsData.filter(comment => comment.user_id === id);
+
+   //This is for blogs length
+   const [blogsData, setBlogsData] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:9292/blogs")
+            .then(resp => resp.json())
+            .then(data => setBlogsData(data));
+   }, [])
+
+   const checkBlogs = blogsData.filter(blog => blog.user_id === id);
+
+   return (
+      <div className="user-div">
+         <h2 className="username-color">{username}</h2>
+         <h4>Total Posts: {checkBlogs.length}</h4>
+         <h4>Total Comments: {checkComments.length}</h4>
+      </div>
+   );
+}
+
+export default User;
---
+++ client/src/Components/Main/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import User from "./User";
+
+function Users({userData}) {
+   const checkUsers = userData.map(user => {
+      return <User
+         key={user.id}
+         username={user.username}
+         id={user.id}
+      />
+   })
+
+   return (
+      {checkUsers}
+   );
+}
+
+export default Users;
