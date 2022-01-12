23123bddd1ddb81bd899f3240f51cb63d2c76620
Eric Yi <erickanginyi@gmail.com>
2022-01-11 21:15:56 -0500

    Create file and import data

---
+++ client/src/Components/Main/Posts.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import Post from "./Post";
+
+function Posts({data, search, searchValue, handleDelete}) {
+   const [userData, setUserData] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:9292/users")
+          .then(resp => resp.json())
+          .then(data => setUserData(data));
+   }, [])
+
+   const postData = data.map(post => {
+      return <Post key={post.id} post={post} userData={userData} handleDelete={handleDelete}/>
+   });
+
+   const filterData = searchValue === "" ? postData : postData.filter(blog => blog.props.post.title.toLowerCase().includes(searchValue.toLowerCase()));
+
+   return (
+      <div>
+         {filterData}
+      </div>
+   );
+}
+
+export default Posts;
