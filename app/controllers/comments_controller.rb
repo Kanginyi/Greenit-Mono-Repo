26fbfbf1dbcfe56599d8e03dbf4d63ab618e4490
Eric Yi <erickanginyi@gmail.com>
2022-01-12 03:14:16 -0500

    Update comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,53 @@
 class CommentsController < ApplicationController
 
-   get "/comments" do
+   def index
       comments = Comment.all
-      comments.to_json
+      render json: comments, status: :ok
    end
 
-   get "/comments/:id" do
-      comment = Comment.find(params[:id])
-      comment.to_json
+   def show
+      comment = Comment.find_by(id: params[:id])
+      if comment
+         render json: comment, status: :ok
+      else
+         render json: {errors: "Comment not found :^("}, status: :not_found
+      end
    end
+
+   def create
+      new_comment = Comment.create(comment_params)
+      if new_comment.valid?
+         render json: new_comment, status: :created
+      else
+         render json: {errors: new_comment.errors.full_messages}, status: :unprocessable_entity
+      end
+   end
+
+   def update
+      comment = Comment.find_by(id: params[:id])
+      comment.update(comment_params)
+      if comment.valid?
+         render json: comment, status: :ok
+      else
+         render json: {errors: comment.errors.full_messages}, status: :unprocessable_entity
+      end
+   end
+
+   def destroy
+      comment = Comment.find_by(id: params[:id])
+      if comment
+         comment.destroy
+         render json: {message: "Comment successfully deleted!"}, status: :no_content
+      else
+         render json: {error: "Comment not found :^("}, status: :not_found
+      end
+   end
+
    
+   private
+
+   def comment_params
+      params.permit(:blog_id, :user_id, :comment_text)
+   end
+
 end
