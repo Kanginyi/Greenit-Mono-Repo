7cb86c2de1ca7f4b0a25d754d45c089068ebcae6
Eric Yi <erickanginyi@gmail.com>
2022-01-12 03:44:29 -0500

    Update user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,53 @@
 class UsersController < ApplicationController
 
-   get "/users" do 
+   def index
       users = User.all
-      users.to_json
+      render json: users, status: :ok
+   end
+   
+   def show
+      user = User.find_by(id: params[:id])
+      if user
+         render json: user, status: :ok
+      else
+         render json: {errors: "User not found :^("}, status: :not_found
+      end
    end
 
-   get "/users/:id" do
-      user = User.find(params[:id])
-      user.to_json
+   def create
+      new_user = User.create(user_params)
+      if new_user.valid?
+         render json: new_user, status: :created
+      else
+         render json: {errors: new_user.errors_full_messages}, status: :unprocessable_entity
+      end
+   end
+
+   def update
+      user = User.find_by(id: params[:id])
+      user.update(user_params)
+      if user.valid?
+         render json: user, status: :ok
+      else
+         render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+      end
+   end
+
+   def destroy
+      user = User.find_by(id: params[:id])
+      if user
+         user.destroy
+         render json: {message: "User successfully deleted!"}, status: :no_content
+      else
+         render json: {error: "User not found :^("}, status: :not_found
+      end
+   end
+
+
+   private
+
+   def user_params
+      params.require(:user).permit(:username, :password)
    end
 
 end
