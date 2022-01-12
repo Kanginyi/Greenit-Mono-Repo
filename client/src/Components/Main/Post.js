82d718bc3e6e954b64dc94e535480483f134d9af
Eric Yi <erickanginyi@gmail.com>
2022-01-11 21:55:22 -0500

    Create file and import code

---
+++ client/src/Components/Main/Post.js
@@ -0,0 +1,108 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
+import {BiUpvote, BiDownvote} from "react-icons/bi";
+
+function Post({post, userData, handleDelete}) {
+   const userObj = userData.filter(user => user.id === post.user.id);
+
+   //Changing date format
+   const date = post.create_at.slice(0, 10);
+   let slash = date.replaceAll("-", "/");
+   let dateFormat = slash.slice(5) + "/" + slash.slice(0, 4);
+   
+   // Handle likes
+   const [likes, setLikes] = useState(0);
+
+   useEffect(() => {
+      fetch(`http://localhost:9292/forum_posts/${post.id}`)
+          .then(resp => resp.json())
+          .then(data => setLikes(data.likes));
+   }, []);
+
+  const handleLikes = () => {
+      fetch(`http://localhost:9292/forum_posts/${post.id}/edit/likes`, {
+         method: "PATCH",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify({likes: likes + 1})
+      })
+         .then(resp => resp.json())
+         .then(data => setLikes(data.likes));
+   }
+
+   // Handle dislikes
+   const [dislikes, setDislikes] = useState(0);
+
+   useEffect(() => {
+      fetch(`http://localhost:9292/forum_posts/${post.id}`)
+         .then(resp => resp.json())
+         .then(data => setDislikes(data.dislikes));
+   }, []);
+
+   const handleDislikes = () => {
+      fetch(`http://localhost:9292/forum_posts/${post.id}/edit/dislikes`, {
+         method: "PATCH",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify({dislikes: dislikes + 1})
+      })
+         .then(resp => resp.json())
+         .then(data => setDislikes(data.dislikes));
+   }
+
+   // Getting the newly created usernames
+   const [newUsernames, setNewUsernames] = useState("");
+
+   useEffect(() => {
+      fetch(`http://localhost:9292/users/${post.user_id}`)
+         .then(resp => resp.json())
+         .then(data => setNewUsernames(data?.username));
+   }, []);
+
+   
+   return (
+      <div className="post-div">
+         
+         <article className="single-post">
+            <div className="user-info">
+               <h5>Posted by <span className="username-color">u/{newUsernames}</span> on {dateFormat}</h5>
+               <button className="delete-X" onClick={() => handleDelete(post.id)}>X</button>
+            </div>
+
+            <div className="post-info">
+               <h3 className="forum-post-title">{post.title}</h3>
+            </div> 
+
+            {post.image_url ? <img src={post.image_url} alt={post.title}/> : null}
+
+            <div>
+               <p>{post.content_post}</p>
+            </div>
+         </article>
+
+         <div>
+            <button
+               onClick={handleLikes}
+               className="upvotes-button"
+            >
+               <BiUpvote className="upvote-fnt"/> {likes} Likes
+            </button>
+               &nbsp;
+            <button
+               onClick={handleDislikes}
+               className="downvotes-button"
+            >
+               <BiDownvote className="dovote-fnt"/> {dislikes} Dislikes
+            </button>
+         </div>
+            <br/>
+
+         <div className="post-btn-section">
+            <Link to={`/users/${post.id}`}>
+               <button className="comment-btn">View Comments</button>
+            </Link>
+         </div>
+      
+      </div>
+   );
+}
+
+export default Post;
---
+++ client/src/Components/Main/PostDetails.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+
+function PostDetails({userData}) {
+   const [onePost, setOnePost] = useState([]);
+   const [isLoaded, setIsLoaded] = useState(false);
+   const [comments, setComments] = useState([]);
+
+   const id = useParams().id;
+   useEffect(() => {
+      fetch(`http://localhost:9292/blogs/${id}`)
+         .then(resp => resp.json())
+         .then(singlePost => {
+            setOnePost(singlePost)
+            setIsLoaded(true)
+         });
+   }, []);
+
+   useEffect(() => {
+      fetch("http://localhost:9292/comments")
+         .then(resp => resp.json())
+         .then(data => setComments(data));
+   }, []);
+
+   const findComments = comments.filter(comment => comment.blog_id === onePost.id);
+
+   const userObj = userData.filter(user => user.id === onePost.user_id);
+
+   const displayComments = findComments.map(foundComment => {
+      const storeUser = userData.filter(user => user.id === foundComment.user_id);
+      
+      return (
+         <div className="comment-section">
+
+            <div className="comment-text-class">
+               <h3>{foundComment.comment_text}</h3>
+            </div>
+
+            <div>
+               <h4 className="comment-username-class">-{storeUser[0]?.username}</h4>
+            </div>
+
+         </div>
+      );
+   });
+
+   if (!isLoaded) {
+      return <h2>Loading...</h2>
+   }
+
+   return (
+      <div className="post-div">
+
+         <article className="single-post">
+            <div className="user-info">
+               <h5>Posted by <span className="username-color">u/{userObj[0]?.username}</span></h5>
+            </div>
+
+            <div className="post-info">
+               <h3 className="forum-post-title">{onePost.title}</h3>
+            </div>
+
+            {onePost.image_url ? <img src={onePost.image_url} alt={onePost.title}/> : null}
+
+            <div>
+               <p>{onePost.content_post}</p>
+            </div>
+         </article>
+
+         <div className="comment-button-class">
+            <button className="upvotes-button">
+               👍 {onePost.likes} Likes
+            </button>
+               &nbsp;
+            <button className="downvotes-button">
+               👎 {onePost.dislikes} Dislikes
+            </button>
+         </div>
+
+         {displayComments}
+         <br/>
+
+      </div>
+   );
+}
+
+export default PostDetails;
