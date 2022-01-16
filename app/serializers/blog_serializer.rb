d8be46a2d4a91b6cfe289266d21e0e4904b197e6
Eric Yi <erickanginyi@gmail.com>
2022-01-15 23:46:06 -0500

    Add :created_at into serializer

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,4 +1,4 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :blog_post, :image_url, :likes, :dislikes
+  attributes :id, :title, :blog_post, :image_url, :likes, :dislikes, :created_at
   has_one :user
 end
