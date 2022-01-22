5b51db99ead64ae073705becdf1e5a7eed710ad6
Eric Yi <erickanginyi@gmail.com>
2022-01-22 04:32:14 -0500

    Import useEffect and useState

--- client/src/Components/Main/Users.js
+++ client/src/Components/Main/Users.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import User from "./User";
 
 function Users({userData}) {
-   const checkUsers = userData?.map(user => <User key={user.id} username={user.username} id={user.id}/>);
+   // Essentially for Blogs and Comments length in child component of User
+   const [blogsData, setBlogsData] = useState([]);
+   const [commentsData, setCommentsData] = useState([]);
+
+   useEffect(() => {
+      fetch("/blogs")
+         .then(resp => resp.json())
+         .then(data =>  setBlogsData(data));
+   }, []);
+
+   useEffect(() => {
+      fetch("/comments")
+      .then(resp => resp.json())
+      .then(data => setCommentsData(data));
+   }, []);
+   
+   const checkUsers = userData?.map(user => <User key={user.id} username={user.username} id={user.id} comments={commentsData} blogs={blogsData}/>);
 
    return (
       <>
