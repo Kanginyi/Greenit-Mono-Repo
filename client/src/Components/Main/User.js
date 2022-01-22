7494cf4c5267e23b1c2559172e11cf73146e1b7f
Eric Yi <erickanginyi@gmail.com>
2022-01-22 04:32:02 -0500

    Remove unused code

--- client/src/Components/Main/User.js
+++ client/src/Components/Main/User.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 function User({username, id, comments, blogs}) {
    const checkComments = comments.filter(comment => comment.user.id === id);
