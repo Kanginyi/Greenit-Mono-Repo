f38629e7739260aa7c2a9059cd38c73d1afccdfc
Eric Yi <erickanginyi@gmail.com>
2022-01-24 16:31:14 -0500

    Create new button, add styling:

--- client/src/Components/Main/CategoryBar.js
+++ client/src/Components/Main/CategoryBar.js
@@ -2,13 +2,33 @@ import React, {useState} from 'react';
 import "../../Stylings/CategoryBar.css";
 import Form from "./Form";
 
+import {useNavigate} from 'react-router-dom';
+
+import {FaSmileBeam} from "react-icons/fa";
+
 function CategoryBar({handleAddPost}) {
    const [showForm, setShowForm] = useState(false);
 
+   let navigate = useNavigate();
+
    return (
       <>
-         <button id="create-post" onClick={() => setShowForm(true)}>+</button>
+         <button
+            id="create-post"
+            onClick={() => setShowForm(true)}
+            title="Create Post"
+         >
+            +
+         </button>
 
+         <button
+            id="all-users"
+            onClick={() => navigate("/users")}
+            title="View All Users"
+         >
+            <FaSmileBeam id="users-smiley"/>
+         </button>
+         
          <Form
             showForm={showForm}
             setShowForm={setShowForm}
