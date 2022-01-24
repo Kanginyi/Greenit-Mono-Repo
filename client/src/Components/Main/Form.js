2235a9d76441a97eae72fc1cebb1b3c35533188c
Eric Yi <erickanginyi@gmail.com>
2022-01-24 00:17:52 -0500

    Update modal aspect

--- client/src/Components/Main/Form.js
+++ client/src/Components/Main/Form.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback, useEffect} from 'react';
+import {useSpring, animated} from "react-spring";
+import "../../Stylings/Form.css";
 
-function Form({setShowForm, handleAddPost}) {
-   const [postForm, setPostForm] = useState({
+function Form({showForm, setShowForm, handleAddPost}) {
+   const [blogForm, setBlogForm] = useState({
       username: "",
       title: "",
       content_post: "",
@@ -11,64 +13,103 @@ function Form({setShowForm, handleAddPost}) {
    });
 
    const handleInputChange = e => {
-      setPostForm({
-         ...postForm,
+      setBlogForm({
+         ...blogForm,
          [e.target.name]:e.target.value
       });
    }
 
    const handleSubmit = e => {
       e.preventDefault()
-      handleAddPost(postForm)
+      handleAddPost(blogForm)
    }
+
+   // Close the modal when clicking outside of the modal
+   const formRef = useRef();
+   const closeForm = e => {
+      if (formRef.current === e.target) {
+         setShowForm(false);
+      }
+   }
+
+   // Close the modal using the "Escape" key
+   const escPress = useCallback(e => {
+      if (e.key === "Escape" && showForm) {
+         setShowForm(false);
+      }
+   }, [showForm, setShowForm]);
+
+   useEffect(() => {
+      document.addEventListener("keydown", escPress);
+      return () => document.removeEventListener("keydown", escPress);
+   }, [escPress]);
+
+   // Animate modal popping up
+   const animation = useSpring({
+      config: {
+         duration: 225
+      },
+      opacity: showForm ? 1: 0,
+      transform: showForm ? `translateY(0%)` : `translateY(-100%)`
+   });
 
 
    return (
-      <div className="modal-background">
-         <div>
-            <div className="x-btn-container">
-               <button className="x-btn" onClick={() => setShowForm(false)}>X</button> 
+      <>
+      {showForm ?
+         <section className="form-background" onClick={closeForm} ref={formRef}>
+            <animated.div style={animation}>
+            <div className="create-post-form">
+               <div>
+                  <button onClick={() => setShowForm(false)}>X</button>
+                  <form className="form-modal" onSubmit={handleSubmit}>
+                     <p>Name <span style={{color: "red"}}>*</span></p>
+                     <input
+                        onChange={handleInputChange}
+                        type="text"
+                        name="username"
+                        value={blogForm.username}
+                        autoComplete="off"
+                        required
+                     />
+
+                     <p>Title <span style={{color: "red"}}>*</span></p>
+                     <input
+                        onChange={handleInputChange}
+                        type="text"
+                        name="title"
+                        value={blogForm.title}
+                        autoComplete="off"
+                        required
+                     />
+
+                     <p>Content <span style={{color: "red"}}>*</span></p>
+                     <textarea
+                        onChange={handleInputChange}
+                        type="text"
+                        name="content_post"
+                        value={blogForm.content_post}
+                        rows="4"
+                        cols="50"
+                     />
+
+                     <p>Image</p>
+                     <input
+                        onChange={handleInputChange}
+                        type="text"
+                        name="image_url"
+                        value={blogForm.image_url}
+                        autoComplete="off"
+                     />
+
+                     <button id="submit-form">Submit!</button>                     
+                  </form>
+               </div>
             </div>
-
-            <form className="form-modal" onSubmit={handleSubmit}>
-               <h4>Name</h4>
-               <input
-                  onChange={handleInputChange}
-                  type="text"
-                  name="username"
-                  value={postForm.username}
-               />
-
-               <h4>Title</h4>
-               <input
-                  onChange={handleInputChange}
-                  type="text"
-                  name="title"
-                  value={postForm.title}
-               />
-
-               <h4>Content</h4>
-               <textarea
-                  onChange={handleInputChange}
-                  type="text"
-                  name="content_post"
-                  value={postForm.content_post}
-                  rows="4"
-                  cols="50"
-               />
-
-               <h4>Image</h4>
-               <input
-                  onChange={handleInputChange}
-                  type="text"
-                  name="image_url"
-                  value={postForm.image_url}
-               />
-
-               <button id="submit-btn">Submit!</button>
-            </form>
-         </div>
-      </div>
+            </animated.div>
+         </section>
+      : null}
+      </>
    );
 }
 
