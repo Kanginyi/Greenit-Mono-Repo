b9d51be2b639e4f4e09ce8590522a2de1526c0de
Eric Yi <erickanginyi@gmail.com>
2022-01-25 02:45:38 -0500

    Fetch comments, add new Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,56 +6,65 @@ import Users from "./Components/Main/Users";
 import PostDetails from "./Components/Main/PostDetails";
 
 import {Route, Routes} from "react-router-dom";
+import UserInfo from "./Components/Main/UserInfo";
 
 function App() {
    const [postData, setPostData] = useState([]);
    const [userData, setUserData] = useState([]);
+   const [commentData, setCommentData] = useState([]);
 
    useEffect(() => {
       fetch("/blogs")
-        .then(resp => resp.json())
-        .then(data => setPostData(data));
-    }, [])
-  
-    const [searchValue, setSearchValue] = useState("");
-  
-    function search(e) {
-      setSearchValue(e.target.value);
-    }
+         .then(resp => resp.json())
+         .then(data => setPostData(data));
+   }, []);
 
    useEffect(() => {
       fetch("/users")
          .then(resp => resp.json())
          .then(data => setUserData(data));
    }, []);
+   
+   useEffect(() => {
+      fetch("/comments")
+         .then(resp => resp.json())
+         .then(data => setCommentData(data));
+   }, []);
+
+   // Search Bar
+   const [searchValue, setSearchValue] = useState("");
+
+   function search(e) {
+   setSearchValue(e.target.value);
+   }
   
-    //FORM SUBMITS
-    const handleAddPost = post => {
-      fetch("/blogs", {
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify(post)
-      })
-      .then(res => res.json())
+   // FORM SUBMITS
+   const handleAddPost = post => {
+   fetch("/blogs", {
+      method: "POST",
+      headers: {
+         "Content-Type" : "application/json",
+      },
+      body: JSON.stringify(post)
+   })
+      .then(resp => resp.json())
       .then(post => {
-        setPostData([
+         setPostData([
          ...postData, post 
-        ])
+         ])
       })
-    }
+   }
   
-    //post delete
-    const handleDelete = id => {
-      fetch(`/blogs/${id}`,{
-        method : 'DELETE'
-      })
-      .then(() => {
-        const deletePost = postData.filter(post => post.id !== id)
-        setPostData(deletePost)
-      })
-    }
+   // Delete Posts
+   const handleDelete = id => {
+   fetch(`/blogs/${id}`,{
+      method : 'DELETE'
+   })
+   .then(() => {
+      const deletePost = postData.filter(post => post.id !== id)
+      setPostData(deletePost)
+   })
+   }
 
   return (
     <div className="App">
@@ -74,13 +83,25 @@ function App() {
                      handleDelete={handleDelete}
                   />
                }/>
+
                <Route path="/users" element={
                   <Users
                      userData={userData}
                      searchValue={searchValue}
                   />
                }/>
-               <Route path="/users/:id" element={<PostDetails userData={userData}/>}/>
+
+               <Route path="/users/:id" element={
+                  <UserInfo
+                     userData={userData}
+                     postData={postData}
+                     commentData={commentData}
+                     searchValue={searchValue}
+                  />
+               }/>
+
+               {/* <Route path="/posts/:id" element={<PostDetails userData={userData}/>}/> */}
+
             </Routes>
       </main>
 
