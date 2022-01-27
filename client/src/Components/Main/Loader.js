1eb5444c0548f7399bafb9439193fe369057d18a
Eric Yi <erickanginyi@gmail.com>
2022-01-27 04:06:42 -0500

    Create file, add code to style

---
+++ client/src/Components/Main/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../Stylings/Loader.css";
+
+function Loader() {
+   return (
+      <div className="loading-ring">
+         LOADING...
+         <span className="loading-span"></span>
+      </div>
+   );
+}
+
+export default Loader;
