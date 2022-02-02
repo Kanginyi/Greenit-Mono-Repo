86799d866d160361a458b08834809a01b246026d
Eric Yi <erickanginyi@gmail.com>
2022-02-02 02:18:37 -0500

    Conditional render on search bar

--- client/src/Components/Header/Navbar.js
+++ client/src/Components/Header/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../../Stylings/Header.css";
 
 import SearchBar from "./SearchBar";
@@ -7,21 +7,32 @@ import LogSign from "./LogSign";
 import greenit_logo from "../../greenit-logo.png";
 
 function Navbar({search}) {
-   return (
-      <nav className="nav">
-         <a href="/">
-            <span
-               title="Greenit >:^("
-               className="logo-box"
-            >
-               <img id="greenit-logo" src={greenit_logo} alt="Greenit Logo"/>
-               <h1>Greenit</h1>
-            </span>
-         </a>
+   const [isClicked, setIsClicked] = useState(false);
 
-         <SearchBar search={search}/>
-         <LogSign/>
-      </nav>
+   const showSearch = () => {
+      setIsClicked(prev => !prev)
+   }
+
+   return (
+      <>
+         <nav className="nav">
+            <a href="/">
+               <span
+                  title="Greenit >:^("
+                  className="logo-box"
+               >
+                  <img id="greenit-logo" src={greenit_logo} alt="Greenit Logo"/>
+                  <h1>Greenit</h1>
+               </span>
+            </a>
+
+            <p onClick={showSearch}>search</p>
+
+            <LogSign/>
+         </nav>
+
+         {isClicked ? <SearchBar search={search}/> : null}
+      </>
    );
 }
 
