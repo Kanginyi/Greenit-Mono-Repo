e0a1c1bb239a49e83336c3a687cfc72f7a89bdf8
Eric Yi <erickanginyi@gmail.com>
2022-02-25 12:32:49 -0500

    Add authentication to user password, render error properly on frontend, hold and comment out test code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,16 @@ class SessionsController < ApplicationController
 
    def create
       user = User.find_by(username: params[:username])
-      session[:user_id] = user.id
-      render json: user
+
+      if user&.authenticate(params[:password])
+         session[:user_id] = user.id
+         render json: user
+      else
+         render json: {error: "Who is this? Invalid username or password >:^("}, status: :unauthorized
+      end
+
+      # session[:user_id] = user.id
+      # render json: user
    end
 
    def destroy
@@ -16,4 +24,41 @@ class SessionsController < ApplicationController
      end
    end
 
+   # def destroy
+   #    session.delete :user_id
+   #    head :no_content
+   # end
+
 end
+
+# def create
+#    user = User.find_by(username: session_params[:username])
+
+#    if user && user.authenticate(session_params[:password])
+#       login!
+#       render json: user, status: :ok
+#    else
+#       render json: {errors: "Who is this? No such user exists."}, status: :not_found
+#    end
+
+#    # session[:user_id] = user.id
+#    # render json: user
+# end
+
+# def destroy
+#    user = current_user
+#    if user
+#       logout!
+#       render json: {message: "See you later! :^)"}, status: :no_content
+#    else
+#       render json: ["Who is this???"], status: :not_found
+#   end
+# end
+
+
+# private
+
+# def session_params
+#    params.require(:user).permit(:username, :password)
+# end
+# end
