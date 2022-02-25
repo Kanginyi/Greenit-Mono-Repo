7adca7f0bc86bef51c800c66e731094deb932502
Eric Yi <erickanginyi@gmail.com>
2022-02-25 15:36:57 -0500

    Take in setCurrentUser as props, create viewProfile function to move to currentUser's info, conditionally render user-section div whether someone's logged in or not

--- client/src/Components/Header/LogSignButtons.js
+++ client/src/Components/Header/LogSignButtons.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
 
-function LogSignButtons({currentUser, setShowSignup}) {
-   let navigate = useNavigate();
+function LogSignButtons({currentUser, setCurrentUser, setShowSignup}) {
+  let navigate = useNavigate();
 
    const toLogin = () => {
       setShowSignup(false);
@@ -16,24 +16,57 @@ function LogSignButtons({currentUser, setShowSignup}) {
       navigate("/welcome");
    }
 
+   // const toLogout = () => {
+   //    fetch("/logout", {
+   //       method: "DELETE"
+   //    })
+   //       // .then(resp => resp.json())
+   //       // .then(data => setCurrentUser(data));
+   // };
+   
+   const viewProfile = () => {
+      navigate(`/users/${currentUser.id}`);
+   }
+
    return (
       <div className="user-section">
+        
+         {
+            currentUser
+         ?
+            // Greetings user|Logout button
+            <>
+               <p>
+                  Hi <span className="username-color" onClick={viewProfile}>
+                     {currentUser.username}
+                  </span>!
+               </p>
 
-         <button
-            id="login-button"
-            onClick={toLogin}
-            type="button"
-         >
-            Login
-         </button>
-         
-         <button
-            id="signup-button"
-            onClick={toSignup}
-            type="button"
-         >
-            Signup
-         </button>
+               <button
+                  id="login-button"
+                  // onClick={toLogout}
+               >
+                  Logout
+               </button>
+            </>
+         :
+            // Signup|Login buttons
+            <>
+               <button
+                  id="signup-button"
+                  onClick={toSignup}
+               >
+                  Signup
+               </button>
+
+               <button
+                  id="login-button"
+                  onClick={toLogin}
+               >
+                  Login
+               </button>
+            </>
+         }
 
       </div>
    );
