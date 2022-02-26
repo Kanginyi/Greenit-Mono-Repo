a6928745752ee2cc532d720d99d0e77f56c4b6b5
Eric Yi <erickanginyi@gmail.com>
2022-02-26 01:03:41 -0500

    Import tons of code, fix all rendering errors before, put in tests

--- client/src/Components/Main/PostDetails.js
+++ client/src/Components/Main/PostDetails.js
@@ -1,18 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import Loader from "./Loader";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
-function PostDetails({userData}) {
-   const [onePost, setOnePost] = useState([]);
+import {BsTrash} from "react-icons/bs";
+
+function PostDetails({currentUser, userData, handleDelete}) {
+   const [blogInfo, setBlogInfo] = useState([]);
    const [isLoaded, setIsLoaded] = useState(false);
    const [comments, setComments] = useState([]);
 
+   let navigate = useNavigate();
    const id = useParams().id;
+
    useEffect(() => {
       fetch(`/blogs/${id}`)
          .then(resp => resp.json())
-         .then(singlePost => {
-            setOnePost(singlePost)
+         .then(blog => {
+            setBlogInfo(blog)
             setIsLoaded(true)
          });
    }, [id]);
@@ -23,63 +27,193 @@ function PostDetails({userData}) {
          .then(data => setComments(data));
    }, []);
 
-   const findComments = comments.filter(comment => comment.blog_id === onePost.id);
+   // Date & Time for the post header
+   const postDate = new Date(blogInfo?.created_at).toLocaleDateString();
+   const postTime = new Date(blogInfo?.created_at).toLocaleTimeString();
 
-   const userObj = userData.filter(user => user.id === onePost.user_id);
+   // Find all comments related to the post
+   const findComments = comments?.filter(comment => comment?.blog?.id === blogInfo?.id);
 
-   const displayComments = findComments.map(foundComment => {
-      const storeUser = userData.filter(user => user.id === foundComment.user_id);
-      
+   // User info
+   const userObj = (userData?.filter(user => user?.id === blogInfo?.user?.id))[0];
+
+   // Render all comments onto the page
+   const displayComments = findComments?.map(foundComment => {
+      const commentUser = (userData?.filter(user => user?.id === foundComment?.user?.id))[0];
+     
       return (
          <div className="comment-section">
 
             <div className="comment-text-class">
-               <h3>{foundComment.comment_text}</h3>
+               <h3>{foundComment?.comment_text}</h3>
             </div>
 
             <div>
-               <h4 className="comment-username-class">-{storeUser[0]?.username}</h4>
+               <h3 className="comment-username-class" style={{cursor: "pointer"}}>
+                  -<span onClick={() => navigate(`/users/${commentUser?.id}`)}>{commentUser?.username}</span>
+               </h3>
             </div>
 
          </div>
       );
    });
 
+   const [isClicked, setIsClicked] = useState(1);
+   const [postLikes, setPostLikes] = useState(blogInfo?.likes);
+   const [postDislikes, setPostDislikes] = useState(blogInfo?.dislikes); 
+   
+   // Likes and Dislikes handling functions
+   const handleLikes = () => {
+      fetch(`/inc_likes/${id}`, {
+         method: "PATCH",
+         headers: {"Content-Type": "application/json"}
+      })
+         .then(resp => resp.json())
+         .then(data => setPostLikes(data.likes));
+
+       if (isClicked === 3) {
+          fetch(`/dec_dislikes/${id}`, {
+             method: "PATCH",
+             headers: {"Content-Type": "application/json"}
+          })
+            .then(resp => resp.json())
+            .then(data => setPostDislikes(data.dislikes));
+       }
+
+       setIsClicked(2);
+   }
+
+   const handleDislikes = () => {
+      fetch(`/inc_dislikes/${id}`, {
+         method: "PATCH",
+         headers: {"Content-Type": "application/json"}
+      })
+         .then(resp => resp.json())
+         .then(data => setPostDislikes(data.dislikes));
+
+      if (isClicked === 2) {
+         fetch(`/dec_likes/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => setPostLikes(data.likes));
+      }
+
+      setIsClicked(3);
+   }
+
+   // Three states of the buttons
+   const notPressed = <>
+                        <button
+                           className="likes-button"
+                           onClick={handleLikes}
+                        >
+                           👍 {postLikes}
+                        </button>
+
+                        <button
+                           className="dislikes-button"
+                           onClick={handleDislikes}                      
+                        >
+                            👎 {postDislikes}
+                        </button>
+                      </>
+
+   const likesPressed = <>
+                        <button
+                           className="likes-pressed"
+                           disabled="disabled"
+                           onClick={handleLikes}
+                        >
+                           👍 {postLikes}
+                        </button>
+
+                        <button
+                           className="dislikes-button"
+                           onClick={handleDislikes}                     
+                        >
+                            👎 {postDislikes}
+                        </button>
+                      </>
+
+   const dislikesPressed = <>
+                        <button
+                           className="likes-button"
+                           onClick={handleLikes}                      
+                        >
+                           👍 {postLikes} 
+                        </button>
+
+                        <button
+                           className="dislikes-pressed"
+                           disabled="disabled"
+                           onClick={handleDislikes}                    
+                        >
+                           👎 {postDislikes}
+                        </button>
+                      </>
+
+   // Loading screen component
    if (!isLoaded) {
       return <Loader/>
    }
 
+   console.log(userObj);
+   
    return (
        <div className="post-div">
 
          <article className="single-post">
             <div className="user-info">
-               <h5>Posted by <span className="username-color">u/{userObj[0]?.username}</span></h5>
+               <h3>
+                  Posted by&nbsp;
+                     <span
+                        className="username-color"
+                        onClick={() => navigate(`/users/${id}`)}
+                        style={{cursor: "pointer"}}
+                     >
+                        u/{userObj?.username}
+                     </span> on {postDate} at {postTime}
+               </h3>
+
+               {currentUser?.username === userObj.username
+                  ? <BsTrash onClick={() => handleDelete(blogInfo?.id)} className="delete-post"/>
+                  : null}
             </div>
 
-            <div className="post-info">
-               <h3 className="forum-post-title">{onePost.title}</h3>
+            <div className="post-header">
+                  <div className="likes-button-container">
+                     {isClicked === 1 ? notPressed :
+                     isClicked === 2 ? likesPressed :
+                     dislikesPressed}
+                  </div>
+                  &nbsp;
+               <h2 className="post-title">{blogInfo?.title}</h2>
             </div>
 
-            {onePost.image_url ? <img src={onePost.image_url} alt={onePost.title}/> : null}
+            {
+               blogInfo?.image_url
+               ? <img src={blogInfo?.image_url} alt={blogInfo?.title}/>
+               : null
+            }
 
             <div>
-               <p>{onePost.content_post}</p>
+               <p>{blogInfo?.blog_post}</p>
             </div>
          </article>
 
          <div className="comment-button-class">
             <button className="upvotes-button">
-               👍 {onePost.likes} Likes
+               👍 {blogInfo?.likes} Likes
             </button>
                &nbsp;
             <button className="downvotes-button">
-               👎 {onePost.dislikes} Dislikes
+               👎 {blogInfo?.dislikes} Dislikes
             </button>
          </div>
 
          {displayComments}
-         <br/>
 
       </div>
    );
