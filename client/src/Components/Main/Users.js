6a27280a1688f5fcd8c6cd535af7d7aaa323d06a
Eric Yi <erickanginyi@gmail.com>
2022-02-27 19:27:45 -0500

    Add Loader functionality

--- client/src/Components/Main/Users.js
+++ client/src/Components/Main/Users.js
@@ -1,15 +1,20 @@
 import React, {useState, useEffect} from 'react';
+import Loader from "./Loader";
 import User from "./User";
 
 function Users({userData, searchValue}) {
    // Essentially for Blogs and Comments length in child component of User
+   const [isLoaded, setIsLoaded] = useState(false);
    const [blogsData, setBlogsData] = useState([]);
    const [commentsData, setCommentsData] = useState([]);
 
    useEffect(() => {
       fetch("/blogs")
          .then(resp => resp.json())
-         .then(data =>  setBlogsData(data));
+         .then(data => {
+            setBlogsData(data);
+            setIsLoaded(true);
+         });
    }, []);
 
    useEffect(() => {
@@ -21,6 +26,11 @@ function Users({userData, searchValue}) {
    const checkUsers = userData?.map(user => <User key={user.id} username={user.username} id={user.id} comments={commentsData} blogs={blogsData}/>);
 
    const filterUsers = searchValue === "" ? checkUsers : checkUsers.filter(user => user.props.username.toLowerCase().includes(searchValue.toLowerCase()));
+
+   // Loading screen component
+   if (!isLoaded) {
+      return <Loader/>
+   }
 
    return (
       <div>
