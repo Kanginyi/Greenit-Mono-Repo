6ed1768d99398767eca5e2dd6bd8bdb57bf6532b
Eric Yi <erickanginyi@gmail.com>
2022-02-28 03:24:21 -0500

    Remove :session_token from validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 
    has_secure_password
 
-   validates :username, :password_digest, :session_token, presence: true
+   # validates :username, :password_digest, :session_token, presence: true
+   validates :username, :password_digest, presence: true
    validates :username, uniqueness: true, length: {maximum: 30}
 
 end
