904ad2dcbd7e7b4da49f9918aaed7bb9955a93ee
Eric Yi <erickanginyi@gmail.com>
2022-03-01 04:28:43 -0500

    Import CSS file, set classNames for styling

--- client/src/Components/Header/LoginSignupForm.js
+++ client/src/Components/Header/LoginSignupForm.js
@@ -2,25 +2,28 @@ import React from 'react';
 import Signup from "./Signup";
 import Login from "./Login";
 
+import "../../Stylings/LoginSignupForm.css";
+
 // Pass down currentUser into this component
 function LoginSignupForm({setCurrentUser, setUserData, showSignup, setShowSignup}) {    
    return (
-      <>
-      {showSignup
-         ? <Signup setCurrentUser={setCurrentUser} setUserData={setUserData}/>
-         : <Login setCurrentUser={setCurrentUser}/>
-      }
+      <div className="login-signup-container">
+            
+         {showSignup
+            ? <Signup setCurrentUser={setCurrentUser} setUserData={setUserData}/>
+            : <Login setCurrentUser={setCurrentUser}/>
+         }
 
-      <div>
-         <p>{showSignup ? "Already have an account?" : "Don't have an account?"}</p>
-         <button
-            onClick={() => setShowSignup(prev => !prev)}
-         >
-            {showSignup ? "Login" : "Signup" }
-         </button>
+         <div className="login-signup-footer">
+            <p>{showSignup ? "Already have an account?" : "Don't have an account?"}</p>
+            <button
+               onClick={() => setShowSignup(prev => !prev)}
+            >
+               {showSignup ? "Login" : "Signup" }
+            </button>
+         </div>
+
       </div>
-
-      </>
    );
 }
 
