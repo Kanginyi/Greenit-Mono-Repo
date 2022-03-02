0b0b436ca8abe8f337ffca59cc9ea6ae604cf221
Eric Yi <erickanginyi@gmail.com>
2022-03-02 17:44:34 -0500

    Create variables commentDate|commentTime to get :created_at data and turn into LocaleDate/TimeStrings and use to show when comments were posted

--- client/src/Components/Main/Comment.js
+++ client/src/Components/Main/Comment.js
@@ -8,13 +8,22 @@ function Comment({comment, userData}) {
 
    const commentUser = (userData?.filter(user => user?.id === comment?.user_id))[0];
 
+   const commentDate = new Date(comment?.created_at).toLocaleDateString();
+   const commentTime = new Date(comment?.created_at).toLocaleTimeString();
+
    return (
       <div className="comment-section">
          <div className="comment-text">
             <p>{comment?.comment_text}</p>
          </div>
 
-         <div>
+         <div className="comment-meta-data">
+            <p className="comment-date-time">
+               <em>
+                  Posted on {commentDate} at {commentTime}
+               </em>
+            </p>
+
             <p className="comment-username">
                -<span onClick={() => navigate(`/users/${commentUser?.id}`)} style={{cursor: "pointer"}}>
                   {commentUser?.username}
