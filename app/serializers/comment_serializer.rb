6b18a0ba9a2665885689a72f9955294d8a1776c4
Eric Yi <erickanginyi@gmail.com>
2022-03-02 17:18:42 -0500

    Add :created_at for comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment_text, :user_id
+  attributes :id, :comment_text, :user_id, :created_at
   has_one :blog
   has_one :user
 end
