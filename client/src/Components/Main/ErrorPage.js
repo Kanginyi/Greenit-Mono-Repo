21ca1a39553f257af14fd5f860f072c7d63834f5
Eric Yi <erickanginyi@gmail.com>
2022-03-10 19:21:17 -0500

    Create file, format page layout

---
+++ client/src/Components/Main/ErrorPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import "../../Stylings/ErrorPage.css";
+import greenit_logo from "../../greenit-logo.png";
+
+function ErrorPage() {
+   let navigate = useNavigate();
+
+   return (
+      <div className="gtfo-logo">
+         <h2>
+            What are you doing here?
+         </h2>
+            <br/><br/>
+         <img
+            src={greenit_logo}
+            alt="Greenit Logo, telling you to get outta here"
+            title="Take me home!!"
+            onClick={() => navigate("/")}
+         />
+            <br/><br/>
+         <h3>
+            You're in the wrong place, Greeniter.
+         </h3>
+            <br/>
+         <h4>Click <span style={{color: "rgb(0, 173, 76)"}}>Greenie</span> to head home~</h4>
+      </div>
+   );
+}
+
+export default ErrorPage;
