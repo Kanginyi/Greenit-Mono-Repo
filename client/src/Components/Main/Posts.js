67f0cb3e71b7f87f96ced0719004ebd3cc1715ca
Eric Yi <erickanginyi@gmail.com>
2022-03-12 09:03:03 -0500

    Take in setPostData as props, pass down postData & setPostData as props into Post component, optional chain in sortPosts

--- client/src/Components/Main/Posts.js
+++ client/src/Components/Main/Posts.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Post from "./Post";
 
-function Posts({currentUser, postData, userData, searchValue, handleDelete}) {
+function Posts({currentUser, postData, setPostData, userData, searchValue, handleDelete}) {
    const renderPosts = postData?.map(post => {
-      return <Post key={post.id} currentUser={currentUser} post={post} userData={userData} handleDelete={handleDelete}/>
+      return <Post key={post.id} currentUser={currentUser} post={post} postData={postData} setPostData={setPostData} userData={userData} handleDelete={handleDelete}/>
    });
 
-   const sortPosts = renderPosts?.sort((a, b) => b.props.post.created_at.localeCompare(a.props.post.created_at));
+   const sortPosts = renderPosts?.sort((a, b) => b?.props?.post?.created_at?.localeCompare(a?.props?.post?.created_at));
 
    const filterPosts = searchValue === "" ? sortPosts : sortPosts.filter(blog => blog.props.post.title.toLowerCase().includes(searchValue.toLowerCase()) || blog.props.post.user.username.toLowerCase().includes(searchValue.toLowerCase()));
 
