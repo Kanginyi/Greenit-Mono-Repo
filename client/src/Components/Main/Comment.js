2be1520c1c1720f053ec7b8e7003668d59cc4ad2
Eric Yi <erickanginyi@gmail.com>
2022-03-17 21:08:41 -0400

    Import useState, FaEdit, create state related to editing comments, add new rendering elements
    Import useState from react, import FaEdit from react-icons
    
    Create showCommentInput and set initial value to false to handle whether or not the edit comment icon is clicked or not
    Create editComment state and set intial value to an object that holds the initial value of the current comment that would potentially be updated
    
    Create handleCommentEdit function that handles the onChange of the newly created input and sets its value to the new edited comment
    Create updaateComment funcion that fetches the initial comment using /comment?.id, passes in the new updatedComment into setCommentData later to update the overall commentData array, and finally changes the showCommentInput value to false
    
    Conditionally render the comment_text based on if showCommentInput was true or not
    If true, change to show an input and a button that would handle all the things related to editing the comment
    => Update defaultValue AND the other side of the coin's comment_text to pull from editComment?.comment_text instead (thank you Sam)
    
    Update comment-date-time's element from p to div
    Add <FaEdit/> to the page and add related actions to it

--- client/src/Components/Main/Comment.js
+++ client/src/Components/Main/Comment.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
 
 import {BsTrash} from "react-icons/bs";
+import {FaEdit} from "react-icons/fa";
 import "../../Stylings/Comment.css";
 
 function Comment({currentUser, comment, userData, commentData, setCommentData}) {
@@ -11,6 +12,38 @@ function Comment({currentUser, comment, userData, commentData, setCommentData}) 
 
    const commentDate = new Date(comment?.created_at).toLocaleDateString();
    const commentTime = new Date(comment?.created_at).toLocaleTimeString();
+
+   const [showCommentInput, setShowCommentInput] = useState(false);
+   const [editComment, setEditComment] = useState({
+      user_id: commentUser?.id,
+      blog_id: comment?.blog?.id,
+      comment_text: comment?.comment_text
+   });
+
+   const handleCommentEdit = e => {
+      setEditComment({
+         ...editComment,
+         user_id: commentUser?.id,
+         [e.target.name]:`EDIT: ${e.target.value}`
+      });
+   };
+
+   const updateComment = () => {
+      fetch(`/comments/${comment?.id}`, {
+         method: "PATCH",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify(editComment)
+      })
+         .then(resp => {
+            if (resp.ok) {
+               resp.json()
+                  .then(updatedComment => {
+                     setCommentData(commentData, updatedComment);
+                     setShowCommentInput(false);
+                  })
+            }
+         })
+   };
 
    const deleteComment = () => {
       let checkDelete = window.confirm("Are you sure you want to delete your comment?");
@@ -24,32 +57,54 @@ function Comment({currentUser, comment, userData, commentData, setCommentData}) 
                setCommentData(deleteComment);
             })
       }
-   }
+   };
 
    return (
       <div className="comment-section">
          <div className="comment-text">
-            <p>{comment?.comment_text}</p>
+            {showCommentInput
+               ? <div className="comment-text-edit">
+                     Currently Editing:
+                     <input
+                     type="text"
+                     name="comment_text"
+                     onChange={handleCommentEdit}
+                     defaultValue={editComment?.comment_text}
+                     autoComplete="off"
+                     spellCheck="false"
+                     required
+                     />
+
+                     <button onClick={updateComment}>Edit Comment</button>
+                  </div>
+                : <p>{editComment?.comment_text}</p>
+            }
          </div>
 
          <div className="comment-meta-data">
-            <p className="comment-date-time">
+            <div className="comment-date-time">
                <em>
                   Posted on {commentDate} at {commentTime}
                </em>
                
                {currentUser?.username === commentUser?.username
-                  ? <>
-                     &nbsp;
+                  ? <div className="comment-user-actions">
+                        &nbsp;
+                     <FaEdit
+                        onClick={() => setShowCommentInput(prev => !prev)}
+                        className="user-edit"
+                        title="Edit Comment"
+                     />
+
                      <BsTrash
                         onClick={deleteComment}
                         className="delete-button"
                         title="Delete Comment"
                      />
-                    </>
+                    </div>
                   : null
                }
-            </p>
+            </div>
 
             <p className="comment-username">
                -<span onClick={() => navigate(`/all_users/${commentUser?.id}`)} style={{cursor: "pointer"}}>
