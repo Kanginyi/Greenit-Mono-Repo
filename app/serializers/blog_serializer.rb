97c440c6ea7ef304e3263846a686d2dc9282aef2
Eric Yi <erickanginyi@gmail.com>
2022-03-21 21:17:28 -0400

    Update attributes to contain related users/comments/blogs

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :blog_post, :image_url, :likes, :dislikes, :created_at
+  attributes :id, :title, :blog_post, :image_url, :likes, :dislikes, :created_at, :user, :comments
   has_one :user
   has_many :comments
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :created_at
+  attributes :id, :username, :created_at, :blogs, :comments
+  has_many :blogs
+  has_many :comments
 end
