18f879dfec0b9cd8438faf7914bcfc02f68295e0
Eric Yi <erickanginyi@gmail.com>
2022-03-23 14:07:58 -0400

    Take in props, update fetch, update passed down props into User component
    Take in setUserData, postData, and commentData as props from parent component
    Remove two useEffects that fetched from /blogs & /comments
    
    Create new useEffect that fetches from /users and use to rerender updated users properly
    
    Update passed down props of comments & blogs to commentData & postData respectively

--- client/src/Components/Users/Users.js
+++ client/src/Components/Users/Users.js
@@ -2,28 +2,19 @@ import React, {useState, useEffect} from 'react';
 import Loader from "../Helpers/Loader";
 import User from "./User";
 
-function Users({userData, searchValue}) {
-   // Essentially for Blogs and Comments length in child component of User
+function Users({userData, setUserData, postData, commentData, searchValue}) {
    const [isLoaded, setIsLoaded] = useState(false);
-   const [blogsData, setBlogsData] = useState([]);
-   const [commentsData, setCommentsData] = useState([]);
 
    useEffect(() => {
-      fetch("/blogs")
+      fetch("/users")
          .then(resp => resp.json())
          .then(data => {
-            setBlogsData(data);
+            setUserData(data);
             setIsLoaded(true);
          });
-   }, []);
-
-   useEffect(() => {
-      fetch("/comments")
-      .then(resp => resp.json())
-      .then(data => setCommentsData(data));
-   }, []);
+   }, [setUserData]);
    
-   const checkUsers = userData?.map(user => <User key={user.id} username={user.username} id={user.id} comments={commentsData} blogs={blogsData}/>);
+   const checkUsers = userData?.map(user => <User key={user.id} username={user.username} id={user.id} comments={commentData} blogs={postData}/>);
 
    const filterUsers = searchValue === "" ? checkUsers : checkUsers.filter(user => user.props.username.toLowerCase().includes(searchValue.toLowerCase()));
 
