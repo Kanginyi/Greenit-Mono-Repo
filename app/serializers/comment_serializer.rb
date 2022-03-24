d13286d811d40f3d66ba061e16df66b3e58fdd5e
Eric Yi <erickanginyi@gmail.com>
2022-03-24 17:24:51 -0400

    Add likes & dislikes to comment_serializer attributes

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment_text, :user, :blog, :created_at
+  attributes :id, :comment_text, :user, :blog, :likes, :dislikes, :created_at
   has_one :blog
   has_one :user
 end
