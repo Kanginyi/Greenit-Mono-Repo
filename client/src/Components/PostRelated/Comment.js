b07d7ddeb8a5f41fa4fe49832cc18dedd39a3ad6
Eric Yi <erickanginyi@gmail.com>
2022-03-27 21:35:01 -0400

    Add comment likes & dislikes related things:
    Import FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp, FaThumbsDown from react-icons/fa
    
    Change initial state of editComment to be destructured object of comment
    Comment out related individually set object keys & values
    
    Import all things related to comment likes & dislikes
    
    Create isClicked state to handle which button state is clicked; set initial value to 1
    Create commentLikes state to handle comment's likes value; set initial value to editComment.likes
    Create commentDislikes state to handle comment's dislikes value; set initial value to editComment.dislikes
    Create commentError state to handle liking/disliking comments errors; set initial value to empty string
    
    Create handleLikes to handle updating related comment's likes
    if currentUser exists, then fetch from 'inc_comment_likes/comment.id' to handle PATCH request, after every Promise returns ok, then setCommentLikes to data.likes, which will render commentLikes to update based on data.likes value
    
    IF isClicked state equals 3, fetch from 'dec_comment_dislikes/comment.id' to handle PATCH request, after every Promise returns okay, then setCommentDislikes to data.dislikes, which will render commentDislikes to update based on data.likes value
    
    Then set isClicked's state value to 2
    
    Else, setCommentError to 'Please login', to render that onto virtual DOM between buttons
    
    Create handleDislikes to handle updating related comment's dislikes
    if currentUser exists, then fetch from 'inc_comment_dislikes/comment.id' to handle PATCH request, after every Promise returns ok, then setCommentDislikes to data.likes, which will render commentDislikes to update based on data.likes value
    IF isClicked state equals 2, fetch from 'dec_comment_likes/comment.id' to handle PATCH request, after every Promise returns okay, then setCommentLikes to data.likes, which will render commentLikes to update based on data.likes value
    
    Then set isClicked's state value to 3
    
    Else, set CommentError to 'Please login', to render that onto virtual DOM between buttons
    
    Create handleUnlike function to handle unliking comment
    Create handleDislike function to handle undisliking comment
    All things are similar to descriptions above
    
    At the end of all, set isClicked's state value to 1
    
    Create notPressed, likesPressed, and dislikesPressed buttons containers, which will render based on isClicked value
    
    Utilize imported Fa icons here, commentError renders here, onClicks will handle related handleLikes, handleDislikes, handleUnlike, and handleUndislike
    
    Create div to encapsulate all of previous comment-meta-data div
    Add div inside to conditionally render based on isClicked value

--- client/src/Components/PostRelated/Comment.js
+++ client/src/Components/PostRelated/Comment.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
 
 import {BsTrash} from "react-icons/bs";
-import {FaEdit} from "react-icons/fa";
+import {FaEdit, FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp, FaThumbsDown} from "react-icons/fa";
 import "../../Stylings/Comment.css";
 
 function Comment({currentUser, comment, userData, commentData, setCommentData}) {
@@ -15,9 +15,12 @@ function Comment({currentUser, comment, userData, commentData, setCommentData}) 
 
    const [showCommentInput, setShowCommentInput] = useState(false);
    const [editComment, setEditComment] = useState({
-      user_id: commentUser?.id,
-      blog_id: comment?.blog?.id,
-      comment_text: comment?.comment_text
+      ...comment
+      // user_id: commentUser?.id,
+      // blog_id: comment?.blog?.id,
+      // comment_text: comment?.comment_text,
+      // likes: comment?.likes,
+      // dislikes: comment?.dislikes
    });
 
    const handleCommentEdit = e => {
@@ -59,6 +62,145 @@ function Comment({currentUser, comment, userData, commentData, setCommentData}) 
       }
    };
 
+   const [isClicked, setIsClicked] = useState(1);
+   const [commentLikes, setCommentLikes] = useState(editComment?.likes);
+   const [commentDislikes, setCommentDislikes] = useState(editComment?.dislikes);
+   const [commentError, setCommentError] = useState("");
+
+   // Likes and Dislikes handling functions
+   const handleLikes = () => {
+      if (currentUser) {
+         fetch(`/inc_comment_likes/${comment?.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => {
+               setCommentLikes(data.likes);
+            });
+
+      if (isClicked === 3) {
+         fetch(`/dec_comment_dislikes/${comment?.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => {
+               setCommentDislikes(data.dislikes);
+            });
+      }
+         setIsClicked(2);
+      } else {
+         setCommentError("Please login");
+      }
+   }
+
+   const handleDislikes = () => {
+      if (currentUser) {
+         fetch(`/inc_comment_dislikes/${comment?.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => {
+               setCommentDislikes(data.dislikes);
+            });
+
+         if (isClicked === 2) {
+            fetch(`/dec_comment_likes/${comment?.id}`, {
+               method: "PATCH",
+               headers: {"Content-Type": "application/json"}
+            })
+               .then(resp => resp.json())
+               .then(data => {
+                  setCommentLikes(data.likes);
+               });
+         }
+            setIsClicked(3);
+      } else {
+         setCommentError("Please login");
+      }
+   }
+
+   const handleUnlike = () => {
+      if (currentUser) {
+         fetch(`/dec_comment_likes/${comment?.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => {
+               setCommentLikes(data?.likes);
+            });
+      }
+      setIsClicked(1);
+   }
+
+   const handleUndislike = () => {
+      if (currentUser) {
+         fetch(`/dec_comment_dislikes/${comment?.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"}
+         })
+            .then(resp => resp.json())
+            .then(data => {
+               setCommentDislikes(data?.dislikes);
+            });
+      }
+      setIsClicked(1);
+   }
+
+   // Three states of the buttons
+   const notPressed = <>
+                        <button
+                           className="likes-button"
+                           onClick={handleLikes}
+                        >
+                           <FaRegThumbsUp/>&nbsp;{commentLikes}
+                        </button>
+
+                        <div className="error-message">{commentError}</div>
+                        
+                        <button
+                           className="dislikes-button"
+                           onClick={handleDislikes}                      
+                        >
+                            <FaRegThumbsDown/>&nbsp;{commentDislikes}
+                        </button>
+                      </>
+
+   const likesPressed = <>
+                        <button
+                           className="likes-pressed"
+                           onClick={handleUnlike}
+                        >
+                           <FaThumbsUp/>&nbsp;{commentLikes}
+                        </button>
+
+                        <button
+                           className="dislikes-button"
+                           onClick={handleDislikes}                     
+                        >
+                            <FaRegThumbsDown/>&nbsp;{commentDislikes}
+                        </button>
+                      </>
+
+   const dislikesPressed = <>
+                        <button
+                           className="likes-button"
+                           onClick={handleLikes}                      
+                        >
+                           <FaRegThumbsUp/>&nbsp;{commentLikes} 
+                        </button>
+
+                        <button
+                           className="dislikes-pressed"
+                           onClick={handleUndislike}                    
+                        >
+                           <FaThumbsDown/>&nbsp;{commentDislikes}
+                        </button>
+                      </>
+
    return (
       <div className="comment-section">
          <div className="comment-text">
@@ -81,36 +223,44 @@ function Comment({currentUser, comment, userData, commentData, setCommentData}) 
             }
          </div>
 
-         <div className="comment-meta-data">
-            <div className="comment-date-time">
-               <em>
-                  Posted on {commentDate} at {commentTime}
-               </em>
-               
-               {currentUser?.username === commentUser?.username
-                  ? <div className="comment-user-actions">
-                        &nbsp;
-                     <FaEdit
-                        onClick={() => setShowCommentInput(prev => !prev)}
-                        className="user-edit"
-                        title="Edit Comment"
-                     />
-
-                     <BsTrash
-                        onClick={deleteComment}
-                        className="delete-button"
-                        title="Delete Comment"
-                     />
-                    </div>
-                  : null
-               }
+         <div className="comment-information">
+            <div className="comment-likes-container">
+               {isClicked === 1 ? notPressed :
+               isClicked === 2 ? likesPressed :
+               dislikesPressed}
             </div>
 
-            <p className="comment-username">
-               -<span onClick={() => navigate(`/all_users/${commentUser?.id}`)} style={{cursor: "pointer"}}>
-                  {commentUser?.username}
-               </span>
-            </p>
+            <div className="comment-meta-data">
+               <div className="comment-date-time">
+                  <em>
+                     Posted on {commentDate} at {commentTime}
+                  </em>
+                  
+                  {currentUser?.username === commentUser?.username
+                     ? <div className="comment-user-actions">
+                           &nbsp;
+                        <FaEdit
+                           onClick={() => setShowCommentInput(prev => !prev)}
+                           className="user-edit"
+                           title="Edit Comment"
+                        />
+
+                        <BsTrash
+                           onClick={deleteComment}
+                           className="delete-button"
+                           title="Delete Comment"
+                        />
+                     </div>
+                     : null
+                  }
+               </div>
+
+               <p className="comment-username">
+                  -<span onClick={() => navigate(`/all_users/${commentUser?.id}`)} style={{cursor: "pointer"}}>
+                     {commentUser?.username}
+                  </span>
+               </p>
+            </div>
          </div>
 
       </div>
