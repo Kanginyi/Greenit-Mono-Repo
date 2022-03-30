99fe3bb3722498048bddeb1b3adb451ab67c8b7f
Eric Yi <erickanginyi@gmail.com>
2022-03-30 17:32:21 -0400

    Update rendered error messages, reorganize and refactor code, add comments throughout code
    Add comments throughout code
    Refactor and reorganize code
    
    Add maxLength={30} to username input
    
    Inside handleSignup function, update the way we render error messages
    With configurations in mind, if errorArray's length is 3, then reorganize the way the error message render onto page
    Move the minimum password error to the front of the array and then reverse array so that username errors show before password errors

--- client/src/Components/LoginSignup/Signup.js
+++ client/src/Components/LoginSignup/Signup.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from "react";
+
 import {useNavigate} from "react-router-dom";
 
 function Signup({setCurrentUser, setUserData}) {
    let navigate = useNavigate();
 
+   // States to handle inputted values for username & password. errorMessage will handle and render any errors related to username & password
    const [username, setUsername] = useState("");
    const [password, setPassword] = useState("");
    const [errorMessage, setErrorMessage] = useState([]);
 
+   // Function handle signing up with new user information. After signing up, setCurrentUser to new user's information, then add that new user object to userData array, and setUserData to that new array.
    const handleSignup = e => {
       e.preventDefault();
 
@@ -21,30 +24,39 @@ function Signup({setCurrentUser, setUserData}) {
                resp.json()
                   .then(user => {
                      setCurrentUser(user);
-                     setUserData(allUsers => [...allUsers, user])
+                     setUserData(allUsers => [...allUsers, user]);
                      navigate("/");
                   })
             } else {
                resp.json()
                   .then(error => {
+                     // Remove "Password digest can't be blank" error message from initial errors array
                      const errorArray = error.errors.filter(error => error !== "Password digest can't be blank");
-                     errorArray.reverse();
+
+                     // With the remaining possible errors, render them so username errors show before password errors
+                     if (errorArray.length === 3) {
+                        const moveToFront = errorArray.pop();
+                        errorArray.unshift(moveToFront);
+                        errorArray.reverse();
+                     }
+
                      setErrorMessage(errorArray);
                   })
             }
          })
    };
 
-   // Show password functionality
-   const [inputPassword, setInputPassword] = useState("password");
+   // State to handle show password functionality
+   const [passwordInput, setPasswordInput] = useState("password");
 
+   // Function to show or hide password when user clicks related checkbox
    const showPassword = () => {
-      if (inputPassword === "password") {
-         setInputPassword("text");
+      if (passwordInput === "password") {
+         setPasswordInput("text");
       } else {
-         setInputPassword("password");
+         setPasswordInput("password");
       }
-   }
+   };
 
    return (
       <div className="signup-container">
@@ -56,13 +68,14 @@ function Signup({setCurrentUser, setUserData}) {
                type="text"
                name="username"
                placeholder="Enter a Username!"
+               maxLength={30}
                value={username}
                onChange={e => setUsername(e.target.value)}
                autoComplete="off"
             />
             
             <input
-               type={inputPassword}
+               type={passwordInput}
                name="password"
                placeholder="Enter a Password!"
                value={password}
