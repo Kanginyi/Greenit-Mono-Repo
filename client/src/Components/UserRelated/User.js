6d7fec4163e6f5782ab6672618608e206a10da22
Eric Yi <erickanginyi@gmail.com>
2022-03-31 02:10:36 -0400

    Refactor and reorganize code, add comments throughout code

--- client/src/Components/UserRelated/User.js
+++ client/src/Components/UserRelated/User.js
@@ -5,17 +5,19 @@ import {useNavigate} from "react-router-dom";
 import "../../Stylings/Users.css";
 
 function User({user, postData, commentData}) {
+   let navigate = useNavigate();
+
+   // Filter through postData & commentData to find all blogs & comments related to each individual user
    const checkBlogs = postData?.filter(blog => blog?.user?.id === user?.id);
    const checkComments = commentData?.filter(comment => comment?.user?.id === user?.id);
 
-   let navigate = useNavigate();
-
-   const checkUserInfo = () => {
+   // Function to route to clicked user's profile when user clicks related div
+   const viewUserInfo = () => {
       navigate(`/all_users/${user?.id}`);
    };
   
    return (
-      <div className="user-div" onClick={checkUserInfo}>
+      <div className="user-div" onClick={viewUserInfo}>
          <h2 className="username-color">
             <span>{user?.username}</span>
          </h2>
