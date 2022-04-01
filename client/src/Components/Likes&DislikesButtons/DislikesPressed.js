4b2b26dfcbce697824fcef165938942bc8f92b36
Eric Yi <erickanginyi@gmail.com>
2022-04-01 19:50:51 -0400

    Take in props, update onClicks
    Take in id, setLikes, setDislikes, clickedNum, setClickedNum as props from parent components
    
    Update first button onClick likesFunction
    --> Add anonymous function and pass in parameters into likesFunction(id, setLikes, setDislikes, clickedNum, setClickedNum)
    
    Update second button onClick undislikeFunction
    --> Add anonymous function and pass in parameters into undislikeFunction(id, setDislikes, setClickedNum)

--- client/src/Components/Likes&DislikesButtons/DislikesPressed.js
+++ client/src/Components/Likes&DislikesButtons/DislikesPressed.js
@@ -2,18 +2,18 @@ import React from "react";
 
 import {FaRegThumbsUp, FaThumbsDown} from "react-icons/fa";
 
-function DislikesPressed({likes, dislikes, likesFunction, undislikeFunction}) {
+function DislikesPressed({id, likes, setLikes, dislikes, setDislikes, likesFunction, undislikeFunction, clickedNum, setClickedNum}) {
    return (
       <>
          <button
-            onClick={likesFunction}
+            onClick={() => likesFunction(id, setLikes, setDislikes, clickedNum, setClickedNum)}
             className="likes-button"
          >
             <FaRegThumbsUp/>&nbsp;{likes}
          </button>
 
          <button
-            onClick={undislikeFunction}
+            onClick={() => undislikeFunction(id, setDislikes, setClickedNum)}
             className="dislikes-pressed"                    
          >
             <FaThumbsDown/>&nbsp;{dislikes}
