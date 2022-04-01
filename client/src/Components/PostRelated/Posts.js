5f001d1f6c632d993df050fa8d8e016e72507c36
Eric Yi <erickanginyi@gmail.com>
2022-04-01 14:28:14 -0400

    Add comments throughout code, reorganize code, remove postData props from being passed into Post child component

--- client/src/Components/PostRelated/Posts.js
+++ client/src/Components/PostRelated/Posts.js
@@ -3,12 +3,21 @@ import React from "react";
 import Post from "./Post";
 
 function Posts({currentUser, userData, postData, searchValue, handleDelete}) {
+   // Map through postData and render each Post component by passing in the related information as props
    const renderPosts = postData?.map(post => {
-      return <Post key={post.id} currentUser={currentUser} post={post} postData={postData} userData={userData} handleDelete={handleDelete}/>
+      return <Post
+               key={post?.id}
+               currentUser={currentUser}
+               post={post}
+               userData={userData}
+               handleDelete={handleDelete}
+             />
    });
 
+   // Sort through renderPosts's Post components and display them from newest to oldest by their "created_at" information
    const sortPosts = renderPosts?.sort((a, b) => b?.props?.post?.created_at?.localeCompare(a?.props?.post?.created_at));
 
+   // If searchValue is an empty string, render all posts inside sortPosts. As searchValue gets updated, check each post's title OR each post author's username to see if they include the inputted searchValue
    const filterPosts = searchValue === "" ? sortPosts : sortPosts.filter(blog => blog.props.post.title.toLowerCase().includes(searchValue.toLowerCase()) || blog.props.post.user.username.toLowerCase().includes(searchValue.toLowerCase()));
 
    return (
