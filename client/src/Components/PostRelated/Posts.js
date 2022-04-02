2364e4b1d7cd806924431fbe7167d4a49dba4fc3
Eric Yi <erickanginyi@gmail.com>
2022-04-02 02:55:51 -0400

    Update variable names

--- client/src/Components/PostRelated/Posts.js
+++ client/src/Components/PostRelated/Posts.js
@@ -4,7 +4,7 @@ import Post from "./Post";
 
 function Posts({currentUser, blogData, searchValue, handleDelete, handlePostLikes, handlePostDislikes, handleUnlikePost, handleUndislikePost}) {
    // Map through blogData and render each Post component by passing in the related information as props
-   const renderPosts = blogData?.map(post => {
+   const renderBlogs = blogData?.map(post => {
       return <Post
                key={post?.id}
                currentUser={currentUser}
@@ -17,11 +17,11 @@ function Posts({currentUser, blogData, searchValue, handleDelete, handlePostLike
              />
    });
 
-   // Sort through renderPosts's Post components and display them from newest to oldest by their "created_at" information
-   const sortPosts = renderPosts?.sort((a, b) => b?.props?.post?.created_at?.localeCompare(a?.props?.post?.created_at));
+   // Sort through renderBlogs's Post components and display them from newest to oldest by their "created_at" information
+   const sortBlogs = renderBlogs?.sort((a, b) => b?.props?.post?.created_at?.localeCompare(a?.props?.post?.created_at));
 
-   // If searchValue is an empty string, render all posts inside sortPosts. As searchValue gets updated, check each post's title OR each post author's username to see if they include the inputted searchValue
-   const filterPosts = searchValue === "" ? sortPosts : sortPosts?.filter(blog => blog?.props?.post?.title?.toLowerCase()?.includes(searchValue?.toLowerCase()) || blog?.props?.post?.user?.username?.toLowerCase()?.includes(searchValue?.toLowerCase()));
+   // If searchValue is an empty string, render all posts inside sortBlogs. As searchValue gets updated, check each post's title OR each post author's username to see if they include the inputted searchValue
+   const filterPosts = searchValue === "" ? sortBlogs : sortBlogs?.filter(blog => blog?.props?.post?.title?.toLowerCase()?.includes(searchValue?.toLowerCase()) || blog?.props?.post?.user?.username?.toLowerCase()?.includes(searchValue?.toLowerCase()));
 
    return (
       <div>
