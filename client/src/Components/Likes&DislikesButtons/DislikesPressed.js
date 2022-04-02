47719a81174fdf5b7590083dc1f05d6f5e32e6e8
Eric Yi <erickanginyi@gmail.com>
2022-04-02 02:25:54 -0400

    Add initial comments

--- client/src/Components/Likes&DislikesButtons/DislikesPressed.js
+++ client/src/Components/Likes&DislikesButtons/DislikesPressed.js
@@ -5,6 +5,7 @@ import {FaRegThumbsUp, FaThumbsDown} from "react-icons/fa";
 function DislikesPressed({id, likes, setLikes, dislikes, setDislikes, likesFunction, undislikeFunction, clickedNum, setClickedNum}) {
    return (
       <>
+         {/* Anonymous function to handle passed in likesFunction. Passing in the required parameters to handle liking posts through handlePostLikes in App.js */}
          <button
             onClick={() => likesFunction(id, setLikes, setDislikes, clickedNum, setClickedNum)}
             className="likes-button"
@@ -12,6 +13,7 @@ function DislikesPressed({id, likes, setLikes, dislikes, setDislikes, likesFunct
             <FaRegThumbsUp/>&nbsp;{likes}
          </button>
 
+         {/* Anonymous function to handle passed in undislikeFunction. Passing in the required parameters to handle undisliking posts through handleUndislikePost in App.js */}
          <button
             onClick={() => undislikeFunction(id, setDislikes, setClickedNum)}
             className="dislikes-pressed"                    
