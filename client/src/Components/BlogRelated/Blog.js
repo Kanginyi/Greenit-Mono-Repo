dae470b144019c7eab1e1f44c5007dc5803cc10f
Eric Yi <erickanginyi@gmail.com>
2022-04-02 22:30:17 -0400

    Add comments throughout code, reorganize and refactor code

--- client/src/Components/BlogRelated/Blog.js
+++ client/src/Components/BlogRelated/Blog.js
@@ -14,31 +14,36 @@ function Blog({currentUser, blog, handleDeleteBlog, handleBlogLikes, handleBlogD
    
    const blogAuthorObj = blog?.user;
 
+   // State to handle whether a blog has been liked or disliked
+   const [clickedNum, setClickedNum] = useState(1);
+   // State to handle blog's likes & dislikes
+   const [blogLikes, setBlogLikes] = useState(blog?.likes);
+   const [blogDislikes, setBlogDislikes] = useState(blog?.dislikes);
+   // State to handle whether "Please login" is shown or not
+   const [loginError, setLoginError] = useState("");
+
    // Function to navigate to blog author's profile when user clicks related username
    const viewUserInfo = () => {
       navigate(`/all_users/${blogAuthorObj?.id}`);
    }
 
-   // Function to navigate to view more information about the clicked blog when user clicked related button
+   // Function to navigate to blog's full page to view more information about the clicked blog when user clicks related button
    const viewMore = () => {
       navigate(`/blogs/${blog?.id}`);
    }
 
-   // Add ... after blogs with more than 100 characters in length
-   let renderOnMainPage;
-   if (blog?.blog_post?.length > 100) {
-      renderOnMainPage = blog?.blog_post?.slice(0, 100) + "...";
-   } else {
-      renderOnMainPage = blog?.blog_post;
-   }
-
+   // Date & Time information for when the blog was created/posted
    const blogDate = new Date(blog.created_at).toLocaleDateString();
    const blogTime = new Date(blog.created_at).toLocaleTimeString();
 
-   const [clickedNum, setClickedNum] = useState(1);
-   const [blogLikes, setBlogLikes] = useState(blog?.likes);
-   const [blogDislikes, setBlogDislikes] = useState(blog?.dislikes);
-   const [loginError, setLoginError] = useState("");
+   // If blog's content is more than 100 characters in length, max it out to 100 characters, add "..." after it, and set shortBlogDesc to the shortened content.
+   // If blog's content is less than 100 characters in length, just set shortBlogDesc to the original content.
+   let shortBlogDesc;
+   if (blog?.blog_post?.length > 100) {
+      shortBlogDesc = blog?.blog_post?.slice(0, 100) + "...";
+   } else {
+      shortBlogDesc = blog?.blog_post;
+   }
 
    return (
       <div className="blog-div">
@@ -56,6 +61,7 @@ function Blog({currentUser, blog, handleDeleteBlog, handleBlogLikes, handleBlogD
                      </span> on {blogDate} at {blogTime}
                </h3>
 
+               {/* If currentUser's username is the same as the blogAuthor's username, give the user the option to delete the blog */}
                {currentUser?.username === blogAuthorObj?.username
                   ? <BsTrash onClick={() => handleDeleteBlog(blog.id)} className="delete-button" title="Delete Post"/>
                   : null}
@@ -108,15 +114,17 @@ function Blog({currentUser, blog, handleDeleteBlog, handleBlogLikes, handleBlogD
 
             <div className="blog-info-underline"></div>
 
+            {/* Only render something if blog's image exists */}
             {blog.image_url ? <img src={blog.image_url} alt={blog.title}/> : null}
 
             <p>
-               {renderOnMainPage}
+               {shortBlogDesc}
             </p>
          </article>
 
          <div className="view-more-container">
             <em>
+               {/* Proper grammar based on the amount of comments */}
                {blog?.comments?.length === 1
                   ? `${blog?.comments?.length} Total Comment`
                   : `${blog?.comments?.length} Total Comments`
