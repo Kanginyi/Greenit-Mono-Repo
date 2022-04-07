1dcf212eaa02f90fccd8f2db671dc03b2a75f360
Eric Yi <erickanginyi@gmail.com>
2022-04-06 21:51:42 -0400

    Update filterBlogs to correctly handle searchValue

--- client/src/Components/UserRelated/UserInfo.js
+++ client/src/Components/UserRelated/UserInfo.js
@@ -230,7 +230,7 @@ function UserInfo({currentUser, setCurrentUser, userData, setUserData, blogData,
          </div>
    });
 
-   const filterBlogs = searchValue === "" ? renderBlogs : renderBlogs?.filter(blog => blog?.props?.children[1]?.props?.children[0]?.props?.title?.toLowerCase()?.includes(searchValue?.toLowerCase()));
+   const filterBlogs = searchValue === "" ? renderBlogs : renderBlogs?.filter(blog => blog?.props?.children[0]?.props?.children[1]?.props?.children[0]?.props?.children?.toLowerCase()?.includes(searchValue?.toLowerCase()));
 
    const filterComments = searchValue === "" ? renderComments : renderComments?.filter(comment => comment?.props?.children[0]?.props?.children[1]?.props?.children?.toLowerCase()?.includes(searchValue?.toLowerCase()));
 
