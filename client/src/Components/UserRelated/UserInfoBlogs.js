3bdd12cd94bb563cc063cca30dc0de19535117fa
Eric Yi <erickanginyi@gmail.com>
2022-04-16 15:27:09 -0400

    Add comments, reorganize and refactor code

--- client/src/Components/UserRelated/UserInfoBlogs.js
+++ client/src/Components/UserRelated/UserInfoBlogs.js
@@ -8,6 +8,8 @@ import placeholder_img from "../../Images/placeholder.png";
 function UserInfoBlogs({blog, currentUser, blogData, setBlogData, commentData, setCommentData, currentUserInfo, userBlogsInfo, setUserBlogsInfo, userCommentsInfo, setUserCommentsInfo}) {
    let navigate = useNavigate();
 
+   // If blog's content is less than 15 characters in length, set blogPost to the original content
+   // If blog's content is more than 15 characters in length, max it out to 15 characters, add "..." after it, and set blogPost to the shortened content
    let blogPost;
    if (blog?.blog_post?.length < 15) {
       blogPost = blog?.blog_post;
@@ -19,28 +21,30 @@ function UserInfoBlogs({blog, currentUser, blogData, setBlogData, commentData, s
    const blogDate = new Date(blog?.created_at).toLocaleDateString();
    const blogTime = new Date(blog?.created_at).toLocaleTimeString();
 
+   // Function to handle deleting related user's blog's using the id of the deleted blog
    const deleteUserBlog = () => {
       let confirmDelete = window.confirm(`Are you sure you want to delete "${blog?.title}"?`);
 
+      // If confirmDelete returns true (because a user clicked confirm), then continue with delete actions
       if (confirmDelete) {
          fetch(`/blogs/${blog?.id}`, {
             method: "DELETE"
          })
             .then(() => {
+               // deleteBlog variable to hold array that removes the deleted blog from userBlogsInfo and setUserBlogsInfo to that new array
                const deleteBlog = userBlogsInfo?.filter(eachBlog => eachBlog?.id !== blog?.id);
-
                setUserBlogsInfo(deleteBlog);
 
-               const deleteRelatedComments = userCommentsInfo?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
-
-               setUserCommentsInfo(deleteRelatedComments);
-
+               // removeBlog variable to hold array that removes the deleted blog from blogData and setBlogData to that new array
                const removeBlog = blogData?.filter(eachBlog => eachBlog?.id !== blog?.id);
-
                setBlogData(removeBlog);
 
-               const removeComments = commentData?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
+               // deletedRelatedComments variable to hold array that removes the deleted blog's comments from userCommmentsInfo and setUserCommentsInfo to that new array
+               const deleteRelatedComments = userCommentsInfo?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
+               setUserCommentsInfo(deleteRelatedComments);
 
+               // removeComments variable to hold array that removes the deleted blog's comments from commentData and setCommentData to that new array
+               const removeComments = commentData?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
                setCommentData(removeComments);
             })
       }
@@ -52,12 +56,14 @@ function UserInfoBlogs({blog, currentUser, blogData, setBlogData, commentData, s
             onClick={() => navigate(`/blogs/${blog?.id}`)}
             className="user-blog-container"
          >
+            {/* If blog.image_url exists, show that image. If not, show the placeholder image */}
             <img src={blog?.image_url ? blog?.image_url : placeholder_img} alt={blog?.title}/>
 
             <div>
                <h4>{blog?.title}</h4>
                <em>{blogPost}</em>
                <p>
+                  {/* Proper grammer based on the amount of likes, dislikes, and comments on the blog */}
                   {blog?.likes === 1 ? `${blog?.likes} Like` : `${blog?.likes} Likes`}
                      &nbsp;|&nbsp;
                   {blog?.dislikes === 1 ? `${blog?.dislikes} Dislike` : `${blog?.dislikes} Dislikes`}
@@ -75,6 +81,7 @@ function UserInfoBlogs({blog, currentUser, blogData, setBlogData, commentData, s
             </div>
          </div>
 
+         {/* If the currentUser's id is the same as the currentUserInfo's id, give the user the options to delete or edit the blog */}
          {currentUser?.id === currentUserInfo?.id
             ?
                <div className="user-info-actions">
