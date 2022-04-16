03095abcff5c92474d854feec65df8c21dcdd51c
Eric Yi <erickanginyi@gmail.com>
2022-04-16 14:57:04 -0400

    Import new component, refactor code, add comments, remove import
    Import newly created UserInfoBlogs component
    Remove placeholder_img import
    
    Add comment for renderBlogs
    Refactor related blog information code into UserInfoBlogs component
    --> Map through sortBlogs and for each blog, render UserInfoBlogs component with related props information

--- client/src/Components/UserRelated/UserInfo.js
+++ client/src/Components/UserRelated/UserInfo.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 
+import UserInfoBlogs from "./UserInfoBlogs";
 import ErrorPage from "../Helpers/ErrorPage";
 import Loader from "../Helpers/Loader";
 
@@ -8,7 +9,6 @@ import "../../Stylings/UserInfo.css";
 
 import {BsTrash} from "react-icons/bs";
 import {FaEdit} from "react-icons/fa";
-import placeholder_img from "../../Images/placeholder.png";
 
 function UserInfo({currentUser, setCurrentUser, userData, setUserData, blogData, setBlogData, commentData, setCommentData, searchValue}) {
    let navigate = useNavigate();
@@ -88,91 +88,21 @@ function UserInfo({currentUser, setCurrentUser, userData, setUserData, blogData,
    // Sort through userBlogsInfo and order them from newest to oldest by their "created_at" information
    const sortBlogs = userBlogsInfo?.sort((a, b) => b?.created_at?.localeCompare(a?.created_at));
 
+   // Map through sortBlogs and render each UserInfoBlogs component by passing in the related information as props
    const renderBlogs = sortBlogs?.map(blog => {
-      let blogPost;
-      if (blog?.blog_post?.length < 15) {
-         blogPost = blog?.blog_post;
-      } else {
-         blogPost = blog?.blog_post?.slice(0, 15) + "...";
-      }
-
-      // Date & Time information for when the blog was created/posted
-      const blogDate = new Date(blog?.created_at).toLocaleDateString();
-      const blogTime = new Date(blog?.created_at).toLocaleTimeString();
-
-      const deleteUserBlog = () => {
-         let confirmDelete = window.confirm(`Are you sure you want to delete "${blog?.title}"?`);
-   
-         if (confirmDelete) {
-            fetch(`/blogs/${blog?.id}`, {
-               method: "DELETE"
-            })
-               .then(() => {
-                  const deleteBlog = userBlogsInfo?.filter(eachBlog => eachBlog?.id !== blog?.id);
-
-                  setUserBlogsInfo(deleteBlog);
-
-                  const deleteRelatedComments = userCommentsInfo?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
-
-                  setUserCommentsInfo(deleteRelatedComments);
-
-                  const removeBlog = blogData?.filter(eachBlog => eachBlog?.id !== blog?.id);
-
-                  setBlogData(removeBlog);
-
-                  const removeComments = commentData?.filter(eachComment => eachComment?.blog?.id !== blog?.id);
-
-                  setCommentData(removeComments);
-               })
-         }
-      }
-
-      return<div className="user-info-blogs">
-               <div
-                  onClick={() => navigate(`/blogs/${blog?.id}`)}
-                  className="user-blog-container"
-               >
-                  <img src={blog?.image_url ? blog?.image_url : placeholder_img} alt={blog?.title}/>
-
-                  <div>
-                     <h4>{blog?.title}</h4>
-                     <em>{blogPost}</em>
-                     <p>
-                        {blog?.likes === 1 ? `${blog?.likes} Like` : `${blog?.likes} Likes`}
-                           &nbsp;|&nbsp;
-                        {blog?.dislikes === 1 ? `${blog?.dislikes} Dislike` : `${blog?.dislikes} Dislikes`}
-                           &nbsp;|&nbsp;
-                        {blog?.comments?.length === 1 ? `${blog?.comments?.length} Comment` : `${blog?.comments?.length} Comments`}
-                     </p>
-                     <p className="blog-comments-footer">
-                        <em>
-                           Posted on&nbsp;
-                           <time dateTime={`${blogDate} ${blogTime}`}>
-                              {blogDate} at {blogTime}
-                           </time>
-                        </em>
-                     </p>
-                  </div>
-               </div>
-
-               {currentUser?.id === currentUserInfo?.id
-                  ?
-                     <div className="user-info-actions">
-                        <BsTrash
-                           onClick={deleteUserBlog}
-                           className="delete-button"
-                           title={`Delete "${blog?.title}"`}
-                        />
-
-                        <FaEdit
-                           onClick={() => navigate(`/editing/${blog?.id}`)}
-                           className="user-edit"
-                           title={`Edit "${blog?.title}"`}
-                        />
-                     </div>
-                  : null
-               }
-            </div>
+      return <UserInfoBlogs
+         blog={blog}
+         currentUser={currentUser}
+         blogData={blogData}
+         setBlogData={setBlogData}
+         commentData={commentData}
+         setCommentData={setCommentData}
+         currentUserInfo={currentUserInfo}
+         userBlogsInfo={userBlogsInfo}
+         setUserBlogsInfo={setUserBlogsInfo}
+         userCommentsInfo={userCommentsInfo}
+         setUserCommentsInfo={setUserCommentsInfo}
+      />
    });
 
    // Sort through userCommentsInfo and order them from newest to oldest by their "created_at" information
