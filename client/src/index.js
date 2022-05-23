2624ce96ab4c0e1bea12a87241372cc3287bbe6d
Eric Yi <erickanginyi@gmail.com>
2022-05-22 23:43:33 -0400

    Import Redux related, wrap Provider around App component

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 
+import store from "./Redux/Store";
+import { Provider } from "react-redux";
+
 import App from './App';
 
 import './index.css';
@@ -9,7 +12,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
       <Router>
-         <App/>
+         <Provider store={store}>
+            <App/>
+         </Provider>
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
