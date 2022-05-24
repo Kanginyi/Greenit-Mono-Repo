ba2007d6844d840677aa72398e3f8e8a7eb63a4b
Eric Yi <erickanginyi@gmail.com>
2022-05-23 23:31:26 -0400

    Update spacing:

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import store from "./Redux/Store";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 
 import App from './App';
 
