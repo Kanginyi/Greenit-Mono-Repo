0ce854ee42ff284fb875740ace4798736c79100e
Eric Yi <erickanginyi@gmail.com>
2022-05-30 17:21:45 -0400

    Import currentUserReducer and add into reducer object

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -2,9 +2,11 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import booleanReducer from "./Features/booleanSlice";
 import passwordInputReducer from "./Features/passwordInputSlice";
+import currentUserReducer from "./Features/currentUserSlice";
 
 export default configureStore({
    reducer: {
+      currentUser: currentUserReducer,
       boolean: booleanReducer,
       passwordInput: passwordInputReducer,
    }
