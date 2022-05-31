e4bf4c69e019ba9a9cf72ccb28eb957e8a09a3d7
Eric Yi <erickanginyi@gmail.com>
2022-05-31 01:32:32 -0400

    Reorganize reducer imports, import searchValueReducer, add to reducer object

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,12 +1,17 @@
 import {configureStore} from "@reduxjs/toolkit";
 
+import currentUserReducer from "./Features/currentUserSlice";
+import searchValueReducer from "./Features/searchValueSlice";
+
 import booleanReducer from "./Features/booleanSlice";
 import passwordInputReducer from "./Features/passwordInputSlice";
-import currentUserReducer from "./Features/currentUserSlice";
+
 
 export default configureStore({
    reducer: {
       currentUser: currentUserReducer,
+      searchValue: searchValueReducer,
+
       boolean: booleanReducer,
       passwordInput: passwordInputReducer,
    }
