810a37b46e0990e70002e9d8dad58968de487e43
Eric Yi <erickanginyi@gmail.com>
2022-05-31 15:30:43 -0400

    Remove booleanSlice related code

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -3,13 +3,9 @@ import {configureStore} from "@reduxjs/toolkit";
 import currentUserReducer from "./Features/currentUserSlice";
 import searchValueReducer from "./Features/searchValueSlice";
 
-import booleanReducer from "./Features/booleanSlice";
-
 export default configureStore({
    reducer: {
       currentUser: currentUserReducer,
       searchValue: searchValueReducer,
-
-      boolean: booleanReducer,
    }
 });
