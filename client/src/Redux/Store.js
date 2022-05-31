8ffc871b245c6bff658a4c24ab48e1af00adc582
Eric Yi <erickanginyi@gmail.com>
2022-05-31 15:39:58 -0400

    Import showSignupReducer, add to reducer object as showSignup

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -2,10 +2,12 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import currentUserReducer from "./Features/currentUserSlice";
 import searchValueReducer from "./Features/searchValueSlice";
+import showSignupReducer from "./Features/showSignupSlice";
 
 export default configureStore({
    reducer: {
       currentUser: currentUserReducer,
       searchValue: searchValueReducer,
+      showSignup: showSignupReducer
    }
 });
