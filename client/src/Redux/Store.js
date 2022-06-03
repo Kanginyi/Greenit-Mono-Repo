6dad6a28a7abe59dba897b997ebc62d5fb08ae55
Eric Yi <erickanginyi@gmail.com>
2022-06-02 20:16:48 -0400

    Import blogDataReducer, add to store reducer object

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 
 import currentUserReducer from "./Features/currentUserSlice";
+import blogDataReducer from "./Features/blogDataSlice";
 import searchValueReducer from "./Features/searchValueSlice";
 import showCreateBlogReducer from "./Features/showCreateBlogSlice";
 import showSignupReducer from "./Features/showSignupSlice";
@@ -8,6 +9,7 @@ import showSignupReducer from "./Features/showSignupSlice";
 export default configureStore({
    reducer: {
       currentUser: currentUserReducer,
+      blogData: blogDataReducer,
       searchValue: searchValueReducer,
       showCreateBlog: showCreateBlogReducer,
       showSignup: showSignupReducer
