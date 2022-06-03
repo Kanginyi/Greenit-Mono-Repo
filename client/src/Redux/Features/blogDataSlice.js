b12e7d3854f9f3e9cf43fb6c12c5c9c6d9dfdb64
Eric Yi <erickanginyi@gmail.com>
2022-06-02 20:18:09 -0400

    Create file, populate with initial blogData code

---
+++ client/src/Redux/Features/blogDataSlice.js
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+   value: []
+};
+
+export const blogDataSlice = createSlice({
+   name: "blogData",
+   initialState,
+    reducers: {
+       setBlogData: (state, action) => {
+          state.value = action.payload;
+       }
+    }
+});
+
+export const {setBlogData} = blogDataSlice.actions;
+
+export default blogDataSlice.reducer;
