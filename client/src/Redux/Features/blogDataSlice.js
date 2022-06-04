b17430e07b8de2fbd827acca3048769e129ab833
Eric Yi <erickanginyi@gmail.com>
2022-06-03 22:27:37 -0400

    Update spacing

--- client/src/Redux/Features/blogDataSlice.js
+++ client/src/Redux/Features/blogDataSlice.js
@@ -7,11 +7,11 @@ const initialState = {
 export const blogDataSlice = createSlice({
    name: "blogData",
    initialState,
-    reducers: {
-       setBlogData: (state, action) => {
-          state.value = action.payload;
-       }
-    }
+   reducers: {
+      setBlogData: (state, action) => {
+         state.value = action.payload;
+      }
+   }
 });
 
 export const {setBlogData} = blogDataSlice.actions;
